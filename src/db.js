5b5e36af48723afbba49bfb0d3636754aadff924
Sergioluis9090 <qfalcons@crece.uss.edu.pe>
2023-08-08 12:36:16 -0500

    CAMBIOS

--- src/db.js
+++ src/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-
+mongoose.set('strictQuery', false);
 ;(async () => {
   try {
+    console.log("QUIERO CONECTARME A LA BD !!!")
     const db = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
